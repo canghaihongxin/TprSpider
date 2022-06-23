d7350fec7e7dfef7f7781403de977834b033b88f
canghaihongxin <canghaihongxin@163.com>
2022-06-23 17:01:36 +0800

    博客园模拟登录

--- NewsSpider/NewsSpider/spiders/cnblogNews.py
+++ NewsSpider/NewsSpider/spiders/cnblogNews.py
@@ -5,6 +5,32 @@ class CnblognewsSpider(scrapy.Spider):
     name = 'cnblogNews'
     allowed_domains = ['news.cnblogs.com']
     start_urls = ['https://news.cnblogs.com/n/recommend']
+    # 只针对当前爬虫设置，
+    custom_settings = {
+        # 让后面的请求引用前面的cookie
+        "COOKIES_ENABLED": True
+    }
+
+    def start_requests(self):
+        # undetected_chromedriver 为开源项目
+        # 入口可以模拟登录拿到cookie, selenium 控制浏览器会被一些网站识别出来，知乎，接勾
+        import undetected_chromedriver.v2 as uc
+        browser = uc.Chrome()
+        browser.get("https://account.cnblogs.com/signin")
+        input("请回车继续")
+        cookie_dict = {}
+        cookies = browser.get_cookies()
+        for cookie in cookies:
+            cookie_dict[cookie['name']] = cookie['value']
+
+        for url in self.start_urls:
+            # 将cookie 交给scrapy,
+            headers = {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                              'Chrome/102.0.5005.63 Safari/537.36 Edg/102.0.1245.33 '
+            }
+            yield scrapy.Request(url, cookies=cookie_dict, headers=headers,
+                                 dont_filter=True)
 
     def parse(self, response):
         title_list = response.xpath('//div[@id="news_list"]//h2/a/text()').extract()
--- NewsSpider/main.py
+++ NewsSpider/main.py
@@ -3,12 +3,13 @@ from scrapy.cmdline import execute
 
 import sys
 import os
+
 # print(os.path.dirname(os.path.abspath(__file__))) #获取当前文件所在的路径
 # os.path.dirname(os.path.abspath(__file__)) 获取当前文件所在的父目录
 
-# 设置执行路径
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+if __name__ == '__main__':
+    # 设置执行路径a
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-# 设置执行命令
-execute(["scrapy", "crawl", "cnblogNews"])
-
+    # 设置执行命令
+    execute(["scrapy", "crawl", "cnblogNews"])
